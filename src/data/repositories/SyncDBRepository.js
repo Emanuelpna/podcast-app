10d12d37055ace38936d9a7292070ef57ee11d12
Emanuel Andrade <emanuelpna.900034017@uniacademia.edu.br>
2024-11-22 11:28:13 -0300

    add logs to sync functions

--- src/data/repositories/SyncDBRepository.js
+++ src/data/repositories/SyncDBRepository.js
@@ -27,10 +27,16 @@ export class SyncDBRepository {
   async uploadLocalDataToCloud() {
     const unsyncedItems = await this.getUnsyncedData()
 
+    LoggingService.log(' -> Iniciando Sincronização de dados do Local para a Cloud')
+
     for await (const item of unsyncedItems) {
       if (!item?.id || !item?.collectionName || !item?.action || !item?.data) continue
 
+      LoggingService.log(`   -> Sincronizando ${item.collectionName}`)
+
       if (item.action === SyncCollectionActions.CREATE_ITEM) {
+        LoggingService.log(`     -> Inserindo novo dado`)
+
         const savedItem = await this._cloudDB.searchByField(item.collectionName, "title", item.data?.title)
 
         if (savedItem.length === 0) {
@@ -38,28 +44,47 @@ export class SyncDBRepository {
         }
       }
 
-      if (item.action === SyncCollectionActions.DELETE_ITEM)
+      if (item.action === SyncCollectionActions.DELETE_ITEM) {
+        LoggingService.log(`     -> Deletando dado`)
+
         await this._cloudDB.removeItem(item.collectionName, item.id)
+      }
+
+      LoggingService.log(`   -> Removendo ${item.collectionName} da fila`)
 
       await this._localDB.removeItem(DatabaseCollectionNames.SYNC_QUEUE, item.id)
     }
+
+    LoggingService.log(' -> Dados o Local faltantes foram salvos na Cloud')
   }
 
   async downloadCloudDataToLocal() {
     const collections = Object.values(DatabaseCollectionNames)
 
+    LoggingService.log(' -> Iniciando Sincronização de dados da Cloud para o Local')
+
     for await (const collection of collections) {
       if (collection === DatabaseCollectionNames.SYNC_QUEUE) continue
+
+      LoggingService.log(`   -> Sincronizando ${collection}`)
+
+      LoggingService.log(`     -> Verificando se existem dados salvos localmente`)
 
       const localItems = await this._localDB.getAllItems(collection)
 
       if (localItems.length > 0) continue
 
-      const cloudItems = await this._cloudDB.getAllItems(collection)
+      LoggingService.log(`     -> Buscando dados da Cloud`)
+
+      const cloudItems = await this._cloudDB.getAllItems(collection, 'title')
+
+      LoggingService.log(`     -> Salvando dados localmente`)
 
       for await (const item of cloudItems) {
         await this._localDB.insertItem(collection, item)
       }
     }
+
+    LoggingService.log(' -> Dados da Cloud faltantes foram salvos localmente')
   }
 }
